44d2f96569c493183eec4f6974c3854933780022
Suralya <Tina@Plavius.de>
2015-06-05 12:53:47 +0200

    Create Unity-Project (3DGame/Soulglitch)

---
+++ 3DGame/Soulglitch/Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+	public int mapSizeX = 28;
+	public int mapSizeY = 27;
+
+	public GameObject TilePrefab;
+	public List<Tile> map = new List<Tile> ();
+
+	public List<Player> userplayers =new List<Player>();
+	public List<Player> enemyplayer = new List<Player>();
+
+	Tile temptile=new Tile();
+
+	// Use this for initialization
+	void Start () {
+		findTiles ();
+		for (int i=map.Count-1; i>=0; i--) {
+			map[i].getNeighbors();
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void findTiles(){
+		var temp = GameObject.FindGameObjectsWithTag ("Tile");
+		for (int i= temp.Length-1; i>=0; i--) {
+
+			temptile= temp[i].GetComponent<Tile>();
+			map.Add (temptile);
+		}
+	}
+}
---
+++ 3DGame/Soulglitch/Assets/Scripts/Tile.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Tile : MonoBehaviour {
+
+	public Vector2 gridPosition = Vector2.zero;
+
+	public int ACost = 1;
+	public bool impassible = false;
+	
+	public List<Tile> neighbors = new List<Tile>();
+
+	public GameManager GM ;
+
+	// Use this for initialization
+	void Start () {
+		gridPosition.x = this.transform.position.x;
+		gridPosition.y = this.transform.position.z;
+
+	}
+	
+	public void getNeighbors(){
+		neighbors=(GM.map.FindAll(
+			delegate(Tile obj) {
+			return   ((gridPosition.x+1==obj.gridPosition.x&&gridPosition.y==obj.gridPosition.y)
+			       ||(gridPosition.x==obj.gridPosition.x&&gridPosition.y+1==obj.gridPosition.y)
+			       ||(gridPosition.x==obj.gridPosition.x&&gridPosition.y-1==obj.gridPosition.y)
+			       ||(gridPosition.x-1==obj.gridPosition.x&&gridPosition.y==obj.gridPosition.y)
+			          );
+		}
+		));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnMouseEnter(){
+		transform.GetComponent<Renderer> ().material.color = Color.blue;
+		Debug.Log ("my position is (" + gridPosition.x +","+gridPosition.y+")");
+	}
+
+	void OnMouseExit(){
+		transform.GetComponent<Renderer>().material.color = Color.white;
+	}
+
+}
