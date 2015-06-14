d1912055b957e105a5e5c6dea3fc9301cb7e5f0f
Tina Plavius <Tina@Plavius.de>
2015-06-14 15:55:25 +0200

    MainMenue added

---
+++ 3DGame/Soulglitch/Assets/Scripts/MainMenue.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenue : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void LoadLvl(){
+		Application.LoadLevel (1);
+	}
+}
