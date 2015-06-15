6a6f2858179bcf62ea5306f57c13a7faac7c6466
Tina Plavius <Tina@Plavius.de>
2015-06-15 16:50:47 +0200

    grey out dead characters (in progress)

---
+++ 3DGame/Soulglitch/Assets/Scripts/Characterbuttons.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Characterbuttons : MonoBehaviour {
+
+	public Text CharOne,CharTwo,CharThree,CharFour;
+	public Button CharacterButtonOne,CharacterButtonTwo,CharacterButtonThree,CharacterButtonFour;
+
+
+	// Use this for initialization
+	void Start () {
+		SetCharacterNames ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//DisableDeadChar ();
+	}
+
+	void SetCharacterNames(){
+		CharOne.text = GameManager.instance.UserPlayers[0].playerName;
+		CharTwo.text = GameManager.instance.UserPlayers[1].playerName;
+		CharThree.text = GameManager.instance.UserPlayers[2].playerName;
+		CharFour.text = GameManager.instance.UserPlayers[3].playerName;
+	}
+
+	void DisableDeadChar(){
+		if (GameManager.instance.UserPlayers [0]==null) {CharacterButtonOne.interactable = false;}
+		if (GameManager.instance.UserPlayers [1]==null) {CharacterButtonTwo.interactable = false;}
+		if (GameManager.instance.UserPlayers [2]==null) {CharacterButtonThree.interactable = false;}
+		if (GameManager.instance.UserPlayers [3]==null) {CharacterButtonFour.interactable = false;}
+	}
+	void HighlightSelected(){
+
+	}
+
+}
