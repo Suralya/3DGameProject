d26ed5d01b7cca749799850cfe53869d0e782779
Suralya <Tina@Plavius.de>
2015-06-21 01:55:32 +0200

    Pausing Game (start for pause menue)

---
+++ 3DGame/Soulglitch/Assets/Scripts/PauseMenue.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class PauseMenue : MonoBehaviour {
+
+	private bool GameisPaused =false;
+	public List<Button> InterfaceButtons= new List<Button>();
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (Input.GetKeyDown(KeyCode.P) && !GameisPaused) {
+			Time.timeScale = 0.0f;
+			GameisPaused = true;
+		} else {
+			Time.timeScale = 1.0f;
+			GameisPaused = false;
+		}
+	}
+
+	public void ShowPauseMenue(){
+
+	}
+
+	public void disableInterface(){
+
+	}
+}
