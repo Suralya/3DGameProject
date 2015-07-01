061ce3b055a06bd8c6ab7828b38ab0482ad3b495
Suralya <Tina@Plavius.de>
2015-07-01 17:14:06 +0200

    Cameramovement via Mouse (needs update later on since corners arent working perfectly)

--- 3DGame/Soulglitch/Assets/Scripts/CameraMovement.cs
+++ 3DGame/Soulglitch/Assets/Scripts/CameraMovement.cs
@@ -1,81 +1,92 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿
+public class CameraMovement : MonoBehaviour
+{
 
-public class CameraMovement : MonoBehaviour {
+    private float _speedfactor = 0.5f;
+    public float StartSpeed = 0.1f;
+    public int Borderwidth = 20;
 
-	private float _speedfactor=0.5f;
-	public float StartSpeed=0.1f;
+    // Use this for initialization
+    void Start()
+    {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKey(KeyCode.W))
-			MoveUp ();
+    }
 
-		if (Input.GetKey (KeyCode.S))
-			MoveDown ();
+    // Update is called once per frame
+    void Update()
+    {
 
-		if (Input.GetKey (KeyCode.A))
-			MoveRight ();
+        //Cameramovement via WASD
+        if (Input.GetKey(KeyCode.W))
+            MoveUp();
 
-		if (Input.GetKey (KeyCode.D))
-			MoveLeft ();
+        if (Input.GetKey(KeyCode.S))
+            MoveDown();
 
-		if (!Input.anyKey) {_speedfactor = StartSpeed;}
-		else {_speedfactor+=0.04f;}
-	
-	}
+        if (Input.GetKey(KeyCode.A))
+            MoveRight();
 
-	private void MoveUp(){
-		Vector3 temp = this.transform.position;
-		temp.z+=1*_speedfactor;
+        if (Input.GetKey(KeyCode.D))
+            MoveLeft();
 
-		this.transform.position=temp;
-	}
-	private void MoveDown(){
-		Vector3 temp = this.transform.position;
-		temp.z-=1*_speedfactor;
-		
-		this.transform.position=temp;
-	}
-	private void MoveRight(){
-		Vector3 temp = this.transform.position;
-		temp.x-=1*_speedfactor;
-		
-		this.transform.position=temp;
-	}
-	private void MoveLeft(){
-		Vector3 temp = this.transform.position;
-		temp.x+=1*_speedfactor;
-		
-		this.transform.position=temp;
-	}
-	private void MoveUpRight(){
-		Vector3 temp = this.transform.position;
-		temp.z-=1*_speedfactor;
-		temp.x-=1*_speedfactor;
-		this.transform.position=temp;
-	}
-	private void MoveUpLeft(){
-		Vector3 temp = this.transform.position;
-		temp.z-=1*_speedfactor;
-		temp.x+=1*_speedfactor;
-		this.transform.position=temp;
-	}
-	private void MoveDownRight(){
-		Vector3 temp = this.transform.position;
-		temp.z+=1*_speedfactor;
-		temp.x-=1*_speedfactor;
-		this.transform.position=temp;
-	}
-	private void MoveDownLeft(){
-		Vector3 temp = this.transform.position;
-		temp.z+=1*_speedfactor;
-		temp.x+=1*_speedfactor;
-		this.transform.position=temp;
-	}
+        if (!Input.anyKey)
+        {
+            //Cameramovement via Mouse
+
+            if (Input.mousePosition.x > Screen.width - Borderwidth)
+            {
+                MoveLeft(); _speedfactor += 0.04f;
+            }
+            else if (Input.mousePosition.x < 0 + Borderwidth)
+            {
+                MoveRight(); _speedfactor += 0.04f;
+            }
+            else if (Input.mousePosition.y > Screen.height - Borderwidth)
+            {
+                MoveUp(); _speedfactor += 0.04f;
+            }
+            else if (Input.mousePosition.y < 0 + Borderwidth)
+            {
+                MoveDown(); _speedfactor += 0.04f;
+            }
+            else
+            {
+                _speedfactor = StartSpeed;
+            }
+        }
+        else { _speedfactor += 0.04f; }
+
+
+
+
+    }
+
+    private void MoveUp()
+    {
+        Vector3 temp = this.transform.position;
+        temp.z += 1 * _speedfactor;
+
+        this.transform.position = temp;
+    }
+    private void MoveDown()
+    {
+        Vector3 temp = this.transform.position;
+        temp.z -= 1 * _speedfactor;
+
+        this.transform.position = temp;
+    }
+    private void MoveRight()
+    {
+        Vector3 temp = this.transform.position;
+        temp.x -= 1 * _speedfactor;
+
+        this.transform.position = temp;
+    }
+    private void MoveLeft()
+    {
+        Vector3 temp = this.transform.position;
+        temp.x += 1 * _speedfactor;
+
+        this.transform.position = temp;
+    }
 }
