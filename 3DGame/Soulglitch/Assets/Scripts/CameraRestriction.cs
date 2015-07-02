cac4c025e58734ea08368faa0d348f2b3c3e3af9
Suralya <Tina@Plavius.de>
2015-07-02 23:41:42 +0200

    Camera Restrictions Added

---
+++ 3DGame/Soulglitch/Assets/Scripts/CameraRestriction.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRestriction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 temp = this.transform.position;
+
+			if(temp.z < 0)
+		{
+			temp.z = 0;
+		}
+		if(temp.z > GameManager.instance.mapSizeY)
+		{
+			temp.z = GameManager.instance.mapSizeY;
+		}
+		
+		if(temp.x < 0)
+		{
+			temp.x = 0;
+		}
+		if(temp.x > GameManager.instance.mapSizeX)
+		{
+			temp.x = GameManager.instance.mapSizeX;
+		}
+
+		this.transform.position = temp;
+	}
+
+
+
+}
