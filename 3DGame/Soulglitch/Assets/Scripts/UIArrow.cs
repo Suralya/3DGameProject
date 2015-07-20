9b150a8e6f15270155eadf46f3f21e37e25804d4
Tina Plavius <Tina@Plavius.de>
2015-07-20 10:39:18 +0200

    Added unfinished UIArrow script - edited scene(arrows)

---
+++ 3DGame/Soulglitch/Assets/Scripts/UIArrow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIArrow : MonoBehaviour
+{
+
+    public Sprite avain;
+    public Sprite avaout;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
