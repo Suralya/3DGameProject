ba33d307cf16c74e942527db03f5df4d46682368
Tina Plavius <Tina@Plavius.de>
2015-08-11 15:48:43 +0200

    added Hotky for camera(arrowkeys)

--- 3DGame/Soulglitch/Assets/Scripts/CameraMovement.cs
+++ 3DGame/Soulglitch/Assets/Scripts/CameraMovement.cs
@@ -24,16 +24,16 @@ public class CameraMovement : MonoBehaviour
     {
 
         //Cameramovement via WASD
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.UpArrow))
             MoveUp();
 
-        if (Input.GetKey(KeyCode.S))
+		if (Input.GetKey(KeyCode.S)||Input.GetKey(KeyCode.DownArrow))
             MoveDown();
 
-        if (Input.GetKey(KeyCode.A))
+		if (Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.LeftArrow))
             MoveRight();
 
-        if (Input.GetKey(KeyCode.D))
+		if (Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.RightArrow))
             MoveLeft();
 
         if (!Input.anyKey)
