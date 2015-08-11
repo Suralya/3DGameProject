6b9be0dbd58688f1c1c8111ff2388f95f6b519d2
Tina Plavius <Tina@Plavius.de>
2015-08-11 16:29:17 +0200

    set prefab for Cursor

---
+++ 3DGame/Soulglitch/Assets/Scripts/CursorChange.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorChange : MonoBehaviour {
+
+	public Texture2D cursorTexture;
+	public CursorMode cursorMode = CursorMode.Auto;
+	public Vector2 hotSpot = Vector2.zero;
+
+
+	/*
+	 void OnMouseEnter() {
+		Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
+	}
+
+	void OnMouseExit() {
+		Cursor.SetCursor(null, Vector2.zero, cursorMode);
+	}
+
+*/
+
+	// Use this for initialization
+	void Start () {
+
+		Cursor.SetCursor (cursorTexture, hotSpot, cursorMode);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
