0e425ed17d6c009236df9f57998a8bac8a99f8b9
Tina Plavius <Tina@Plavius.de>
2015-08-13 15:08:17 +0200

    Curser change on diferent actions- edit Apbar color

--- 3DGame/Soulglitch/Assets/Scripts/CursorChange.cs
+++ 3DGame/Soulglitch/Assets/Scripts/CursorChange.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class CursorChange : MonoBehaviour {
 
 	public Texture2D cursorTexture;
+	public Texture2D cursorTextureattack;
+	public Texture2D cursorTexturemove;
 	public CursorMode cursorMode = CursorMode.Auto;
 	public Vector2 hotSpot = Vector2.zero;
 
@@ -28,6 +30,13 @@ public class CursorChange : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (GameManager.instance.UserPlayers [GameManager.instance.currentPlayerIndex].attacking || GameManager.instance.UserPlayers [GameManager.instance.currentPlayerIndex].aiming) {
+			Cursor.SetCursor (cursorTextureattack, hotSpot, cursorMode);
+		} else if (GameManager.instance.UserPlayers [GameManager.instance.currentPlayerIndex].moving) {
+			Cursor.SetCursor (cursorTexturemove, hotSpot, cursorMode);
+		}
+		else{Cursor.SetCursor (cursorTexture, hotSpot, cursorMode);}
 	
 	}
 }
